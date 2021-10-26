2e4bfef20f50f49974d57de7fec9be44d30c27a7
Muyiwa <muyiwasalau@gmail.com>
2021-10-26 09:11:44 -0700

    change of code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,26 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-unsigned int i, j, temp;
-if (size < 2 || !array)
-return;
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-print_array(array, size);
-}
-}
-}
+	int swap = 1, tmp = 0;
+	size_t i;
+
+	if (size < 2 || !array)
+		return;
+
+	while (swap == 1)
+	{
+		swap = 0;
+
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+	}
 }
